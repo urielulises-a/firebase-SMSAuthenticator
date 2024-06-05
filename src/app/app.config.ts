b01934af314c01d6d5d37bf314cb1b329409d889
uriel <uriel.ao@live.com>
2024-06-05 15:57:27 -0600

    Configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,13 +8,8 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, provideAuth } from '@angular/fire/auth'; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBraHqXbIuzNnLciVbuwzS_akXHWt67xC4",
-  authDomain: "angularfireaut.firebaseapp.com",
-  projectId: "angularfireaut",
-  storageBucket: "angularfireaut.appspot.com",
-  messagingSenderId: "359433537495",
-  appId: "1:359433537495:web:2f78d1c528a625ff8aed92",
-  measurementId: "G-6V9WPMRFPG"
+
+  //Your config goes here
 };
 
 
